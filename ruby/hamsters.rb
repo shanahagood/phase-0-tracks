b94849e02b5b0ac27c41592e7086995f8cd7aa54
“Shana <realstarterv@gmail.com>
2016-08-23 15:11:17 -0700

    Print hamster data

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,20 @@
 puts "What is the hamster's name?"
 ham_nam = gets.chomp
 puts "What is your volume level? (1-10)"
-vol_level = gets.chomp
+vol_level = gets.chomp.to_i
 puts "What is the hamster's fur color?"
 fur_color = gets.chomp
 puts "Is this hamster a good candidate for adoption (y/n)?"
 candidacy = gets.chomp
 puts "Age"
-ham_age = gets.chomp
-	if ham_age == ""
+ham_age = gets.chomp.to_i
+	if ham_age == 0
 		ham_age = nil
 	else 
 		ham_age = ham_age
 	end 
+puts "Name: #{ham_name}"
+puts "Volume level: #{vol_level}"
+puts "Fur color: #{fur_color}"
+puts "Adoptable: #{candidacy}"
+puts "Age: #{ham_age}"
