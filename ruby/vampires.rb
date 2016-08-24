8a4f876e7427ab08fcf70d9578ba2659a06a13d6
“Shana <realstarterv@gmail.com>
2016-08-24 11:05:31 -0700

    Write questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,7 @@
+p "May I please have your name?"
+user_name = gets.chomp
+p "How old are you? What year were you born?"
+age_and_birthplace =gets.chomp
+p "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+garlic_bread = gets.chomp
+p "Would you like to enroll in the company's health insurance?"
