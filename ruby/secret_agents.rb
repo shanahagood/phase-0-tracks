1631c245002e1ea583a059a05e77bcc45bb56c6a
“Shana <realstarterv@gmail.com>
2016-08-26 12:19:34 -0700

    add driver code, debug lines 26 and 50

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,14 +1,65 @@
-#encrypt method that advances every letter of a string one letter forward 
-#- index and select a string
+#Encript method advances each letter by 1; assume lower case; must maintain spaces
+
+# add variable for word to be encrypted
+# word = "drink"
+#anticipate: esjol
+
+#def method & establish an index
+def encrypt(word)
 index = 0
-encrypt_string = "pokemon"
 
-
-#- advance string +1 
-while index < encrypt_string.length
-  encrypt_string[index] = encrypt_string[index].next
-  index += 1  
+# advance each letter by 1 in the alphabet
+while index < word.length
+	word[index] = word[index].next
+	index += 1
 end
 
-#- print and test
-p encrypt_string
+if word.include? " "
+	word.sub(/["!"]/, '" "')
+end
+	
+#return the new word
+p word
+
+end
+
+
+
+# decrypt(encrypt("swordfish"))
+# word = "drink"
+
+#def decrypt method - reverse the process to return original password
+def decrypt(word)
+	
+#index letters in alphabet
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	letter = 0
+	
+#index (word) to find each letter's index
+while letter < word.length
+	if word[letter] != " "
+		word[letter] = alphabet[alphabet.index(word[letter]) -1]
+	end
+	
+letter +=1
+end
+
+p word
+end
+
+
+
+#add driver code to ask user encrypt/decrypt? then password name.
+puts "Would you like to encrypt or decrypt a password?"
+
+ 	answer = gets.chomp
+
+puts "What is the password?"
+
+	word = gets.chomp
+
+if answer == "encrypt"
+  encrypt(word)
+else
+  decrypt(word)
+end
