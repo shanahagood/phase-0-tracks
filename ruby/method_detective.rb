869fba28eaac23ad5018da6115906c9c36148e20
“Shana <realstarterv@gmail.com>
2016-08-29 14:33:11 -0700

    Add methods to lines 13, 16, 25, 31, 34

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,16 +4,16 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".
 # => “InVeStIgAtIoN”
 
 # "zom".<???>
 # => “zoom”
 
-# "enhance".<???>
+ "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+ "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
 # "the usual".<???>
@@ -22,16 +22,16 @@
 # " suspects".<???>
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+ "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
