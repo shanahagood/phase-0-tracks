197cc6bef67cf3205568b9d9a9fe65d4f9fefc32
“Shana <realstarterv@gmail.com>
2016-08-30 09:09:02 -0700

    Add questions about client, prompt user input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,6 @@
 #Pseudocode for client details program 
 
-#-"client info" equals empty array.
+#-"client info" equals empty hash.
 #-Ask/Prompt client for name. 
 #-Ask/Prompt client for age.
 #-Ask/Prompt client for number of children they have.
@@ -15,3 +15,23 @@
 #----Print updated "client info." 
 #--If answer is "no":
 #----Print out a thank you message. 
+
+client_info = {} 
+
+puts "May I please have your name?"
+cl_name = gets.chomp
+client_info["name"] = cl_name
+
+puts "May I please have your age?"
+cl_age = gets.chomp
+client_info["Age"] = cl_age
+
+puts "Would you please tell me the number of children you have?"
+num_children = gets.chomp
+client_info["Number of Children"] = num_children
+
+puts "Which decor theme do you prefer?"
+dec_theme = gets.chomp
+client_info["Decor theme"] = dec_theme 
+
+puts client_info
