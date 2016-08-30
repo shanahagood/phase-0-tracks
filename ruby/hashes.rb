f14a63b1e2f290251d66dcfae5e281f0a79b0ceb
“Shana <realstarterv@gmail.com>
2016-08-29 20:10:21 -0700

    Add in pseudocode for program

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,17 @@
-#Code for client details program 
+#Pseudocode for client details program 
+
+#-"client info" equals empty array.
+#-Ask/Prompt client for name. 
+#-Ask/Prompt client for age.
+#-Ask/Prompt client for number of children they have.
+#-Ask/Prompt client for decor theme
+#-Print "client info"
+#-Ask client if there is anything they would like to update.
+#-Prompt answer. 
+#--If answer is "yes":
+#----Ask client what they would like to update. 
+#----Provide list of options.
+#----Prompt client for option. 
+#----Print updated "client info." 
+#--If answer is "no":
+#----Print out a thank you message. 
