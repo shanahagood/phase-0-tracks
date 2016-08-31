b16ac11ce743eda0b502decda4304b4a431d2de2
“Shana <realstarterv@gmail.com>
2016-08-31 15:37:31 -0700

    Add data to data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,19 +1,44 @@
 music = {
 	rock_band: {
-		band_name: ,
+		band_name: "JAM Project",
 		band_info: {
-			total_members: ,
-			total_albums:
+			total_members: 6,
+			total_albums: 17
 		},
 		members: [
-
+			"Masami Okui",
+			"Hironobu Kageyama",
+			"Yoshiki Fukuyama",
+			"Hiroshi Kitadani",
+			"Masaaki Endoh"
 		]
 
 	},
 	pop_band: {
+		band_name: "Walkure",
+		band_info: {
+			total_members: 5,
+			total_albums: 1
+		},
+		members: [
+			"Mikmo Geynemer",
+			"Freyja Wion",
+			"Kaname Buccaneer",
+			"Makina Nakajima",
+			"Reina Prowler"
+		]
 
 	},
-	hip_hop_band: {
+	country_band: {
+		band_name: "The Dixie Chicks",
+		band_info: {
+			total_members: 3,
+			total_albums: 10
+		},
+		members: [
+			"Martie Erwin Maguire",
+			"Emily Erwin Robinson.",
+					]
 
 	}
 
