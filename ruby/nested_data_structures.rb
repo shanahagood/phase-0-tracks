09c6c6009576506178b93a59122496ec39bed930
“Shana <realstarterv@gmail.com>
2016-08-31 18:31:56 -0700

    Add example for changing an item in a hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -44,22 +44,26 @@ music = {
 
 }
 
-#Test 1-Access specified band member name by index
+#Example 1-Access element in an array by index
 p music[:pop_band][:members][2]
 
-# Test 2-Access specified hash
-music[:rock_band][:band_info] 
+#Example 2-Access a hash
+p music[:rock_band][:band_info] 
 
-#Test 4-Edit data inside an index
+#Example 3-Change an item in a hash
+music[:rock_band][:band_name]=("Fairy Empire")  
+p music [:rock_band]
+
+#Example 4-Change an item inside an array
 music[:pop_band][:members][0]="Mikumo Geynemer"
 puts music[:pop_band][:members][0]
 p music[:pop_band][:members]
 
-#Test 5-Add data to an index
+#Example 5-Add an item to an array by index
 music[:country_band][:members].push("Natalie Maines")
 puts music[:country_band][:members]
 p music[:country_band][:members]
 
-#Test 6-Delete data by index
+#Example 6-Delete an item in an array by index
 music[:rock_band][:members].delete_at(0)  
 p music [:rock_band][:members]
