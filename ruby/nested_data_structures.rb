b2291dffa5ea2b2ae40d16b839759d007a16420e
“Shana <realstarterv@gmail.com>
2016-08-31 17:27:21 -0700

    Add example on accessing data from an index

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -44,16 +44,19 @@ music = {
 
 }
 
-#Test 1-Edit data inside an index
+#Test 1-Access specified band member name by index
+p music[:pop_band][:members][2]
+
+#Test 4-Edit data inside an index
 music[:pop_band][:members][0]="Mikumo Geynemer"
 puts music[:pop_band][:members][0]
 p music[:pop_band][:members]
 
-#Test 2-Add data to an index
+#Test 5-Add data to an index
 music[:country_band][:members].push("Natalie Maines")
 puts music[:country_band][:members]
 p music[:country_band][:members]
 
-#Test 3-Delete data by index
+#Test 6-Delete data by index
 music[:rock_band][:members].delete_at(0)  
 p music [:rock_band][:members]
