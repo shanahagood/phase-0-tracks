724d8e08cfb419908e67526a7dc981143e2f6255
“Shana <realstarterv@gmail.com>
2016-09-02 08:32:51 -0700

    Add pseudocode for algorithm that creates a fake name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,7 @@
+#Method that creates a fake name
+#Ask user to input their first and last name. 
+#Have the first name and last name swap places with each other. 
+#  -If there are vowels present:
+#    -Change vowel to the next vowel in 'aeiou'
+#  -If there are consonants:
+# 	 -Change consonant to the next consonant in the alphabet. 
