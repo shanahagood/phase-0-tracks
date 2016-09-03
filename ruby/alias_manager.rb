164d8f1837023e86f3b3be14911bba61003e981a
“Shana <realstarterv@gmail.com>
2016-09-03 11:23:45 -0700

    Add code asking user for first and last name, then swapping those names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,6 +2,18 @@
 #Ask user to input their first and last name. 
 #Have the first name and last name swap places with each other. 
 #  -If there are vowels present:
-#    -Change vowel to the next vowel in 'aeiou'
+#    -Advance each vowel by one in 'aeiou'
 #  -If there are consonants:
-# 	 -Change consonant to the next consonant in the alphabet. 
+# 	 -Advance each consonant by one in the alphabet. 
+
+#Ask user for first name
+puts "What is your first name?"
+first_name = gets.chomp 
+
+#Ask user for last name
+puts "What is your last name?"
+last_name = gets.chomp
+
+
+swap_name = last_name, first_name.gsub(first_name, last_name) 
+puts "Swapped name is #{swap_name}!"  
