73f92e49f75e2d1813df9a60d9455856d61ceca9
“Shana <realstarterv@gmail.com>
2016-09-06 10:29:56 -0700

    Add a puppy class along with characteristicsa nd behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+#SPECIES--------------------
+#fluffyus furus
+
+#CHARACTERISTICS------------
+#Eye count: two
+#Nose: one
+#Ears: two
+#Tail: one 
+#Legs: four
+#Mouth: one
+#Friendly: yes
+#Color: varies
+#Name: varies
+
+#BEHAVIOR--------------------
+#Jump
+#Run
+#Tailwag 
+#Hug
+#Pant 
