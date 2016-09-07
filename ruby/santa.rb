5735e5870a6387ce8b7fc581eedf8b5ac0c15dd7
“Shana <realstarterv@gmail.com>
2016-09-07 08:54:38 -0700

    Add eat_milk_and_cookies method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,8 +7,14 @@ class Santa
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
+
+  def eat_milk_and_cookies(cookie_type)
+  	puts "That was a good #{cookie_type}!"
+  end 
+
 end
 
 santa = Santa.new
 santa.speak
+santa.eat_milk_and_cookies("snickerdoodle")
 
