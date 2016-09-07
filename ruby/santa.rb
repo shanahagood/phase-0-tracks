a1633df08dcfc5e7afcb84ce0b48f21bc184e12a
“Shana <realstarterv@gmail.com>
2016-09-07 08:49:30 -0700

    Add initialize method, add call to instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,14 @@
 class Santa
 
+  def initialize
+  	puts "Initializing new Santa instance....!"
+  end 
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
 end
 
 santa = Santa.new
+santa.speak
 
