9d8b372eec06a1b54db4ce68f176480a13065be2
“Shana <realstarterv@gmail.com>
2016-09-07 19:05:19 -0700

    Add pseudocode for adding items to list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,26 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Add desired item(s)
+  # set default quantity
+  # print the list to the console [print cart]
+# output: hash of items
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: prompt user for item and quantity
+# output: print a message that confirms the item and quantity
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
