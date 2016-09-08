116a06f94d65cacabe87ed42ef8d541575f5f70e
“Shana <realstarterv@gmail.com>
2016-09-07 17:53:22 -0700

    Add get_mad_at(deer_name) method, place reindeer_ranking in initialize

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,10 @@
 class Santa
 
-  def initialize(gender, ethnicity) 
+  def initialize(gender) 
   	puts "Initializing Santa instance...!"
   	@gender = gender 
-  	@ethnicity = ethnicity
+  	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
+  	"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end 
 
   def speak
@@ -12,11 +13,6 @@ class Santa
 
   def eat_milk_and_cookies(cookie_type)
   	puts "That was a good #{cookie_type}!"
-  end 
-
-  def ranking
-	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
-  	"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end 
 
 #getter method for age attribute
@@ -28,14 +24,36 @@ class Santa
   def celebrate_birthday
 	@age += 1 
   end 
-end
+#moves reindeer name to bottom of ranking
+  #perhaps figure out the index 
+
+  # def reindeer(deer_name)
+  # 	@reindeer = deer_name
+  # end 
+
+#   def get_mad_at(deer_name)
+#   	@deer_name = @reindeer_ranking
+#   end 
+
+  def reindeer(deer_name)
+   	@reindeer = deer_name
+   end 
+
+  def get_mad_at(deer_name)
+    @reindeer_ranking.delete(deer_name) 
+    @reindeer_ranking.push(deer_name)
+  end 
+
+  def ethnicity(race)
+	@ethnicity = race
+  end
+end  
 
 
-
-santa = Santa.new("female", "black",)
+santa = Santa.new("female")
 santa.speak
 santa.eat_milk_and_cookies("snickerdoodle")
 santa.age(18)
-# puts santa.age(18)
 santa.celebrate_birthday 
-
+santa.ethnicity("Black") 
+p santa.get_mad_at("Vixen")
