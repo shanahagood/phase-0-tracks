3879fbee0098da15c70033854f4d419990ecd48a
“Shana <realstarterv@gmail.com>
2016-09-09 12:04:23 -0700

    Add pseudocode for class and attribute readers

---
+++ ruby/wordguess/wordguess_pseudo.rb
@@ -0,0 +1,7 @@
+#Word guessing-game pseudo 
+#----
+#Define a class for the game 
+#  -Set a readable attribute for "guess count"
+#  -Set a readable attribute for "game over"
+#Define initialize method to start the game
+# 
