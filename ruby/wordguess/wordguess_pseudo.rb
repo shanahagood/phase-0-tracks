2db37bf97e75c126b3289b9cf13d0c366c2f4aa0
“Shana <realstarterv@gmail.com>
2016-09-09 15:20:41 -0700

    define guess count method

--- ruby/wordguess/wordguess_pseudo.rb
+++ ruby/wordguess/wordguess_pseudo.rb
@@ -2,6 +2,18 @@
 #----
 #Define a class for the game 
 #  -Set a readable attribute for "guess count"
-#  -Set a readable attribute for "game over"
+#  -Set a readable attribute for "game over" to false.
 #Define initialize method to start the game
-# 
+#  -"word" attribute is equal to user input or an array of varied names. 
+#  -"guess count" is equal to the word length
+#    -#determine word length by counting each letter
+#  -""
+#End method
+#Define "guess count method"
+#  -"guess count" minus equals 1. 
+#    -If "word" is the same as/equal to "word" length:
+#      -"game over" is false
+# 	 -Else:
+#      -"game over" is true
+#End method
+#End class
