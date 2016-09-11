5aa748221a24f0a7c48b70689e979e5dc2241730
“Shana <realstarterv@gmail.com>
2016-09-11 13:51:44 -0700

    Add code worked on during gps to file

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -25,10 +25,48 @@
 # steps: iterate through each item in hash and print the key and values
 # output: print entire cart (hash)
 
-def shopping_cart(items, quantity)
+def shopping_cart(items)
 	cart = {}
-	cart[items] = quantity
+	array = items.split(" ")
+
+	array.each do |item|
+		cart[item] = 1
+	end	
 	cart 
 end
 
-puts shopping_cart("carrots apples cereal pizza", 5)
+puts shopping_cart("carrots apples cereal pizza")
+
+our_cart = shopping_cart("carrots apples cereal pizza")
+
+
+def add_item(cart,item, quantity)
+	cart[item] = quantity
+	cart
+end
+
+
+
+def remove_item(cart, item)
+	cart.delete(item)
+	p cart
+	cart
+end
+
+#remove_item(our_cart, "apples")
+
+def update_item(cart, item, quantity)
+	cart[item] = quantity
+	cart
+end
+
+#update_item(our_cart, "pizza", 3)
+
+def print_list(cart)
+	cart.each do |item, quantity|
+		puts "We have #{quantity} #{item} in your cart"
+	end
+	cart
+end
+
+add_item(our_cart, "oranges", 2)
