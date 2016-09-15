8c7a153ef68fe88bf34d7235bdc7a50770faedcc
“Shana <realstarterv@gmail.com>
2016-09-15 12:29:57 -0700

    Edit pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,7 @@
 //define function called reverse that takes one string as an argument.
-//love => evol
-//
+//Iterate through each letter in the string. 
+//example string: love => evol
+// first index becomes 4th/last index
+//2nd index becomes 3rd index
+//3rd index becomes 2nd index
+//4th index becomes 1st index
