1763d109bb2e057d5f328374ed34e9792cc8e991
“Shana <realstarterv@gmail.com>
2016-09-15 13:29:02 -0700

    Add purple to colors array, add Javascript to names array

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,8 @@
 var colors = ["green", "blue", "yellow", "red"];
 var names = ["Sparkles", "Jack", "Mocha", "Ruby"];
+
+colors.push("purple");
+names.push("Javascript");
+
+console.log(colors);
+console.log(names);
