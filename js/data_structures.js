2af92bba38f55e377046193b3390c87963dae61c
“Shana <realstarterv@gmail.com>
2016-09-15 14:21:41 -0700

    Add car constructor function with properties and function

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,33 @@ for (i = 0; i < names.length; i++)
 	{horse[names[i]]=colors[i];
 	}
 console.log(horse);
+
+function Car(model, color, isFastCar ){
+console.log ("Our new car:", this);
+this.model = model;
+this.color = color;
+this.isFastCar = isFastCar;
+this.beep = function(){console.log("Beeeeep!");
+};
+console.log ("Car itialization complete");
+}
+
+console.log ("Let's choose a new car...");
+
+var anotherCar = new Car ("Mitsubishi", "silver", true);
+
+console.log(anotherCar);
+
+console.log("Our car can beep!");
+anotherCar.beep();
+
+console.log("*******************");
+
+var oneMoreCar = new Car ("Honda", "red", false);
+
+console.log(oneMoreCar);
+
+console.log("This car can beep also!");
+oneMoreCar.beep();
+
+console.log("*******************");
