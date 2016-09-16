5cfa515f0c0d441324137210556859b52d86bbb1
“Shana <realstarterv@gmail.com>
2016-09-16 12:14:58 -0700

    Add more pseudocode, start building function

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,23 @@
-//define function called reverse that takes one string as an argument.
-//Iterate through each letter in the string. 
+//define function called reverse that takes one string as an argument. 
 //example string: love => evol
 // first index becomes 4th/last index
 //2nd index becomes 3rd index
 //3rd index becomes 2nd index
 //4th index becomes 1st index
+//  The index reads from smallest to highest so now
+//  we want the index to read from highest to lowest.
+// 1, 2, 3, 4, 5 needs to become 5, 4, 3, 2, 1
+
+//Split string into an array.
+//For each letter in the array:
+//	find the current index
+//Arrange index number from highest to lowest. 
+  
+
+
+function reverseString(string) {
+	return string;
+}
+
+
+
