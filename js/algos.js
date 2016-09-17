9aea89eccc1dc2aa38df86418a9cdbc4ce24605e
“Shana <realstarterv@gmail.com>
2016-09-16 22:04:07 -0700

    Pseudocode release 0

---
+++ js/algos.js
@@ -0,0 +1,21 @@
+//Release 0
+// write a function that takes an array of words or phrases and returns the longest 
+// word or phrase in the array. So if we gave your function the array of 
+// ["long phrase","longest phrase","longer phrase"], it would return "longest phrase"
+
+
+//Define a function called "rtrn_longest_p" that takes "array" as its argument. 
+//For each string in the array:
+//  index is equal to zero. 
+//	Until the index is equal to the length of the string:
+//		index is equal to index plus one. 
+//	print index 
+//Sort and arrange index of each string from largest to smallest.
+//	If index is the first index:
+//  Print "(string) is the longest phrase."
+//End method
+//Test method by calling "rtrn_longest_p" and designating an array as its argument.
+//----------------------------
+//
+
+
