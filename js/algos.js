bdf45228c5f62295901ec4dd0b9d8c0c181f5d19
“Shana <realstarterv@gmail.com>
2016-09-18 16:58:01 -0700

    Edit pseudocode

--- js/algos.js
+++ js/algos.js
@@ -1,10 +1,10 @@
 //Release 0
 // write a function that takes an array of words or phrases and returns the longest 
 // word or phrase in the array. So if we gave your function the array of 
-// ["long phrase","longest phrase","longer phrase"], it would return "longest phrase"
+// ["long phrase","longest phrase","longer phrase"], it would return "longest phrase."
 
-//Define a function called "return longest phrase" that takes "array" as its argument. 
-//Split array 
+//Define a function called "return longest phrase" that takes an array as its argument. 
+//Split each word into its own array
 //Determine the length of each string.
 //For each item/string in the array:
 //  index is equal to zero. 
@@ -19,10 +19,12 @@
 //Test method by calling "rtrn_longest_p" and designating an array as its argument.
 //
 
-// function returnLongestPhrase(phrase1, phrase2, phrase3)
-// for (var index = 0; index < phrase1.length; index++){
-// 	console.log(index);
-// 	}
+function returnLongestPhrase(array)
+for (var index = 0; index < array.length; index++){
+	console.log(index);
+}
+
+
 // 	var newIndex = phraseArray.sort(function);
 // 	// if (newIndex){
 
@@ -39,7 +41,7 @@
 // 	// console.log(phraseArray);
 // }
 
-// console.log(rtrnLongestPhrase(phraseArray));
+// console.log(returnLongestPhrase(["long phrase", "longest phrase", "longer phrase"]));
 // ---------------------------------------------------
 //Release 1
 // write a function that takes two objects and checks to see if the objects share at 
@@ -50,4 +52,18 @@
 // return false. To make your life easier, don't worry about whether a property is a 
 // string ('age') or an identifier name (age). 
 
-
+//Define a function called "share check" that takes two key-value pairs as arguments (pair 1, pair 2).
+//Access each hash's key-value.  
+//hash -> key symbol -> key value.
+//Print pair 1's key-value, print pair 2's key-value.
+//  If pair 1's key is the same as pair 2's key:
+//	  print "true."
+//	Else:
+//	  print "false"
+//  If pair 2's value is the same as pair 2's value:
+//	  print "true"
+//	Else:
+//	  print "false"
+//
+//
+//	
