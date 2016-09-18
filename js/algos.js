fa3d56738d307eef39d8089dd290e1751bb5ebb9
“Shana <realstarterv@gmail.com>
2016-09-18 13:07:43 -0700

    Edit code and test

--- js/algos.js
+++ js/algos.js
@@ -3,19 +3,41 @@
 // word or phrase in the array. So if we gave your function the array of 
 // ["long phrase","longest phrase","longer phrase"], it would return "longest phrase"
 
-
-//Define a function called "rtrn_longest_p" that takes "array" as its argument. 
-//For each string in the array:
+//Define a function called "return longest phrase" that takes "array" as its argument. 
+//Split array 
+//Determine the length of each string.
+//For each item/string in the array:
 //  index is equal to zero. 
-//	Until the index is equal to the length of the string:
+//	While index is lower than the length of the string:
 //		index is equal to index plus one. 
 //	print index 
 //Sort and arrange index of each string from largest to smallest.
+//  If   
 //	If index is the first index:
 //  Print "(string) is the longest phrase."
 //End method
 //Test method by calling "rtrn_longest_p" and designating an array as its argument.
-//----------------------------
 //
 
+function returnLongestPhrase(phrase1, phrase2, phrase3)
+for (var index = 0; index < phrase1.length; index++){
+	console.log(index);
+	}
+// 	var newIndex = phraseArray.sort(function);
+// 	// if (newIndex){
+
+// 	// }
+// 	console.log(newIndex)
+	// if (index == 0){
+	// 	console.log("is the longest phrase!")
+	
+
+
+// function rtrnLongestPhrase(array) {
+// 	var index = 0;
+
+// 	// console.log(phraseArray);
+// }
+
+// console.log(rtrnLongestPhrase(phraseArray));
 
