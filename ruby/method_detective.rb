ff516e9f579cee34cab5f031ec366d42e15bf01b
“Shana <realstarterv@gmail.com>
2016-09-22 11:15:17 -0400

    Add .count a to line 38 and uncomment to test output

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -35,5 +35,5 @@
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".???
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
