1ba2358fe8a1142371dfe9049c09cd9ea1cf5781
“Shana <realstarterv@gmail.com>
2016-09-23 11:42:09 -0400

    Add pseudocode for conditional loop

--- ruby/hashes_chipmunks.rb
+++ ruby/hashes_chipmunks.rb
@@ -19,3 +19,18 @@
 #Print "What decor theme do you prefer? Type anything you wish!"
 #Prompt user for input
 #"decor theme input" is equal to "client theme"
+#Print "Is there anything you would like to update? (yes/no)"
+#Prompt user for input.  
+# => If "answer" is equal to "yes":
+# =>   Print "What would you like to update?"
+# =>   Print ""Type in one of these options: Name | Age | Number of Children | Decor Theme"
+# =>   Prompt user to type in option.
+# =>     case "option":
+# =>   		When "name", then print "Please update!"
+# =>        "client info""name" = input prompt.
+# =>  		Print "Name has been updated to 'client info''name'!"
+# =>     End 
+# => Elsif "answer" is equal to "no":
+# =>   print "Nothing to update!"
+#End 
+# Print "client info"
