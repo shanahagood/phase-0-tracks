dee09840b8e44b579d30d5fc2551c80a68649199
“Shana <realstarterv@gmail.com>
2016-09-23 10:10:14 -0400

    Add pseudocode for computer logic and user interface

---
+++ ruby/hashes_chipmunks.rb
@@ -0,0 +1,21 @@
+#PSEUDOCODE
+#--COMPUTER LOGIC--
+#"client info" is equal to empty hash. 
+#"client info""name" is equal to "client name"
+#"client info""age" is equal to "client age"
+#"client info""number of children" is equal to "client children"
+#"client info""decor theme" is equal to "client theme"
+#Print out "client info"
+#--USER INTERFACE--
+#Print "What is your name?"
+#Prompt user for input. 
+#"name_input" is equal to "client name"
+#Print "What is your age"
+#Prompt user for input.
+#"age_input" is equal to "client age"
+#Print "How many children do you have?"
+#Prompt user for input
+#"children_input" is equal to "client children"
+#Print "What decor theme do you prefer? Type anything you wish!"
+#Prompt user for input
+#"decor theme input" is equal to "client theme"
