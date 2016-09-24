ff44e2e2bf109693d02cf41a298783bc4f9131ad
“Shana <realstarterv@gmail.com>
2016-09-24 16:09:00 -0400

    Add an array called dishes and a hash called dirty_or_clean

--- ruby/iteration_chipmunks.rb
+++ ruby/iteration_chipmunks.rb
@@ -1,7 +1,17 @@
 #RELASE 0
-def greeting
-	puts "Hello, my friend!"
-	yield ("Sierra")
-end 
+# def greeting
+# 	puts "Hello, my friend!"
+# 	yield ("Sierra")
+# end 
 
-greeting { |name| puts "It's been a while, #{name}!" }
+# greeting { |name| puts "It's been a while, #{name}!" }
+
+#Release 1
+dishes = ["fork", "spoon", "plate", "cup"]
+p dishes
+
+dirty_or_clean = { 
+	washed: 1,
+	unwashed: 2
+}
+p dirty_or_clean
