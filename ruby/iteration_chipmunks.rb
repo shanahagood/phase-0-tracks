0355023aec0feb04ca4351528bc56929f022137c
“Shana <realstarterv@gmail.com>
2016-09-24 17:34:56 -0400

    Add method that deletes nubmers if they are lower than 5

--- ruby/iteration_chipmunks.rb
+++ ruby/iteration_chipmunks.rb
@@ -7,23 +7,29 @@
 # greeting { |name| puts "It's been a while, #{name}!" }
 
 #Release 1
-dishes = ["fork", "spoon", "plate", "cup"]
-p dishes
+# dishes = ["fork", "spoon", "plate", "cup"]
+# p dishes
 
-dirty_or_clean = { 
-	washed: 1,
-	unwashed: 2
-}
-p dirty_or_clean
+# dirty_or_clean = { 
+# 	washed: 1,
+# 	unwashed: 2
+# }
+# p dirty_or_clean
 
-dishes.each do |dish_type|
-  puts "This is a #{dish_type}!"
-end 
+# dishes.each do |dish_type|
+#   puts "This is a #{dish_type}!"
+# end 
 
-dishes.map! do |dish_type|
-  puts dish_type * 2 
-end 
+# dishes.map! do |dish_type|
+#   puts dish_type * 2 
+# end 
 
-dirty_or_clean.each do |state_of_dish, number|
-	puts "#{number} dishe(s) is/are #{state_of_dish}."
-end 
+# dirty_or_clean.each do |state_of_dish, number|
+# 	puts "#{number} dishe(s) is/are #{state_of_dish}."
+# end 
+
+# p dirty_or_clean.map { |item| item[1] }
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+numbers.delete_if {|number| number < 5} 
+p numbers  
