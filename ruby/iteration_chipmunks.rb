a67437b1e384c3cbbbb4bbdd0a7efed47225246f
“Shana <realstarterv@gmail.com>
2016-09-24 19:42:09 -0400

    Add method that prints words until it finds one whose length is equal to 4

--- ruby/iteration_chipmunks.rb
+++ ruby/iteration_chipmunks.rb
@@ -1,44 +1,44 @@
 #RELASE 0
-# def greeting
-# 	puts "Hello, my friend!"
-# 	yield ("Sierra")
-# end 
+def greeting
+	puts "Hello, my friend!"
+	yield ("Sierra")
+end 
 
-# greeting { |name| puts "It's been a while, #{name}!" }
+greeting { |name| puts "It's been a while, #{name}!" }
 
 #Release 1
-# dishes = ["fork", "spoon", "plate", "cup"]
-# p dishes
+dishes = ["fork", "spoon", "plate", "cup"]
+p dishes
 
-# dirty_or_clean = { 
-# 	washed: 1,
-# 	unwashed: 2
-# }
-# p dirty_or_clean
+dirty_or_clean = { 
+	washed: 1,
+	unwashed: 2
+}
+p dirty_or_clean
 
-# dishes.each do |dish_type|
-#   puts "This is a #{dish_type}!"
-# end 
+dishes.each do |dish_type|
+  puts "This is a #{dish_type}!"
+end 
 
-# dishes.map! do |dish_type|
-#   puts dish_type * 2 
-# end 
+dishes.map! do |dish_type|
+  puts dish_type * 2 
+end 
 
-# dirty_or_clean.each do |state_of_dish, number|
-# 	puts "#{number} dishe(s) is/are #{state_of_dish}."
-# end 
+dirty_or_clean.each do |state_of_dish, number|
+	puts "#{number} dishe(s) is/are #{state_of_dish}."
+end 
 
-# p dirty_or_clean.map { |item| item[1] }
+p dirty_or_clean.map { |item| item[1] }
 
 #Delete any numbers lower than 5
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-# numbers.delete_if {|number| number < 5} 
-# p numbers  
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+numbers.delete_if {|number| number < 5} 
+p numbers  
 
 #Keeps any numbers lower than 5. 
-# num_array = [3, 4, 5, 6, 7]
-# new_array = num_array.reject { |item| item >= 5 }
-# p new_array 
+num_array = [3, 4, 5, 6, 7]
+new_array = num_array.reject { |item| item >= 5 }
+p new_array 
 
 #Adds "is awesome" to a word if its length is greater than 4 
 # array = ["music", "love", "travel"]
@@ -50,9 +50,16 @@
 
 # puts array 
 
-#Method that divides a number by 2 if even 
-# num_array = [1, 2, 3, 4]
-# even_array = num_array.collect do |num| 
-#  if num.even? then num / 2  
-# end 
-# end
+Method that divides a number by 2 if even 
+num_array = [1, 2, 3, 4]
+even_array = num_array.collect do |num| 
+ if num.even? then num / 2  
+end 
+end
+
+#Print word until word length = 4
+word_array = ["happiness", "joy", "love", "bliss"]
+word_array.map! do |word|
+ p word
+ break if word.length == 4
+end
