e1ea0cd5bebb0536ed43cf086a7848c89e2e5548
“Shana <realstarterv@gmail.com>
2016-09-25 16:01:35 -0700

    Add example on how to access array element by index

--- ruby/nested_data_structures_chipmunks.rb
+++ ruby/nested_data_structures_chipmunks.rb
@@ -1,37 +1,42 @@
-Anime = {
-	sports: {
-		series_name: 'Haikyuu', 
-		series_info: {
-			episodes: 30
-			seasons: 1
-		},
-		fav_characters: [
-			"Shoyo Hinata", 
-			"Tobio Kageyama", 
-			"Yuu Nishinoya"
-		] 
+anime = {
+    sports: {
+        series_name: 'Haikyuu',
+        series_info: {
+            episodes: 30,
+            seasons: 1
+        },
+        fav_characters: [
+            "Shoyo Hinata", 
+            "Tobiko Kageyama", 
+            "Yuu Nishinoya"
+        ]
 	},	
-	sci_fi: {
-		series_name: 'Macross Frontier', 
-		series_info: {
-			episodes: 26
-			seasons: 1
-		},
-		fav_characters: [
-			"Mikumo Geynemer", 
-			"Freyja Wion", 
-			"Kaname Bucaneer"
-		] 
+    sci_fi: {
+        series_name: 'Macross Frontier',
+        series_info: {
+            episodes: 26,
+            seasons: 1
+        },
+        fav_characters: [
+            "Mikumo Geynemer", 
+            "Freyja Wion", 
+            "Kaname Bucaneer"
+        ]
 	},
 	magical_girl: {
-		series_name: 'Sailor Moon', 
-		series_info: {
-			episodes: 200
-			seasons: 5
-		},
-		fav_characters: [
-			"Usagi Tsukino", 
-			"Seiya Yaten", 
-			"Haruka Tennou"
-		] 
+        series_name: 'Sailor Moon',
+        series_info: {
+            episodes: 200,
+            seasons: 5
+        },
+        fav_characters: [
+            "Usagi Tsukino", 
+            "Seiya Yaten", 
+            "Haruka Tennou"
+        ]
+	}
 }
+
+#Example 1-Access element in an array by index
+puts anime[:sports][:fav_characters][1]
+
