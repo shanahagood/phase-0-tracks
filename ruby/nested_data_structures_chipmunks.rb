c20ea4984d04d46c87fa8765abc9a0d6a37b086a
“Shana <realstarterv@gmail.com>
2016-09-25 15:14:21 -0700

    Add sci_fi genre to data structure with nested items

--- ruby/nested_data_structures_chipmunks.rb
+++ ruby/nested_data_structures_chipmunks.rb
@@ -1,18 +1,28 @@
 Anime = {
 	sports: {
-	series_name: 'Haikyuu', 
+		series_name: 'Haikyuu', 
 		series_info: {
 			episodes: 30
 			seasons: 1
 		},
-		main_characters: [
+		fav_characters: [
 			"Shoyo Hinata", 
 			"Tobio Kageyama", 
 			"Yuu Nishinoya"
 		] 
-
 	},	
-
+	sci_fi: {
+		series_name: 'Macross Frontier', 
+		series_info: {
+			episodes: 26
+			seasons: 1
+		},
+		fav_characters: [
+			"Mikumo Geynemer", 
+			"Freyja Wion", 
+			"Kaname Bucaneer"
+		] 
+	}
 	
 	
 
