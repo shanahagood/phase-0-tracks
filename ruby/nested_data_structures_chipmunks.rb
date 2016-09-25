a17b98540efd5964bb6409b60aef1a4e1aae18f2
“Shana <realstarterv@gmail.com>
2016-09-25 15:02:39 -0700

    Build data structure called Anime with genres, series name, and characters

---
+++ ruby/nested_data_structures_chipmunks.rb
@@ -0,0 +1,20 @@
+Anime = {
+	sports: {
+	series_name: 'Haikyuu', 
+		series_info: {
+			episodes: 30
+			seasons: 1
+		},
+		main_characters: [
+			"Shoyo Hinata", 
+			"Tobio Kageyama", 
+			"Yuu Nishinoya"
+		] 
+
+	},	
+
+	
+	
+
+
+}
