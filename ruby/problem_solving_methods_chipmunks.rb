2d4e921c4b063ae87918495147d53058700de4d1
“Shana <realstarterv@gmail.com>
2016-09-26 09:13:48 -0700

    Add release 1 problem

---
+++ ruby/problem_solving_methods_chipmunks.rb
@@ -0,0 +1,11 @@
+# Write a method that takes an array of integers and an integer to search for. The method 
+#should return the index of the item, or nil if the integer is not present in the array. 
+#Don't use built-in array methods like .index. You are allowed to use .length and .each.
+
+# Here's an example of how we might use your method (assuming you call it search_array):
+
+# arr = [42, 89, 23, 1]
+# p search_array(arr, 1)
+# => 3
+# p search_array(arr, 24)
+# => nil
