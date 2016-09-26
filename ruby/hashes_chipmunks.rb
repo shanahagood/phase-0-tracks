c4b47646d803a9ac0d2d4faeaedf2f1ff4b8d283
“Shana <realstarterv@gmail.com>
2016-09-26 06:48:26 -0700

    Add contionals for if option is equal to a, c, or d

--- ruby/hashes_chipmunks.rb
+++ ruby/hashes_chipmunks.rb
@@ -38,24 +38,48 @@ answer = gets.chomp
 if answer == "yes"
 # =>Print "What would you like to update?"
 	puts "What would you like to update?"
-# =>Print ""Type in one of these options: Name | Age | Number of Children | Decor Theme"
-	puts "Type in one of these options: Name | Age | Number of Children | Decor Theme"
+# =>Print "Type in one of these options: Name | Age | Number of Children | Decor Theme"
+	puts "Type in one of these options: 'n' for Name | 'a' for Age | 'c' for Number of Children | 'd' for Decor Theme"
 # =>Prompt user to type in option.
 	option = gets.chomp
 # =>if option is equal to name: 
-	if option == "name" 
+	if option == "n" 
 # =>   	Print "Please update!"
 		puts "Please update!"
-# =>    "client info""name" = input prompt.
+# =>    "client info: name" is equal to input prompt.
 		client_info[:client_name] = gets.chomp
-# =>    Print "Name has been updated to 'client info''name'!"
+# =>    Print "Name has been updated to "client info: client name"
 		puts "Name has been updated to #{client_info[:client_name]}!"
-# =>    End conditional
-		end 
-# => Elsif "answer" is equal to "no":
-	elsif answer == "no"
-# =>   print "Nothing to update!"
+# => Elsif option is equal to "a"
+	elsif option == "a" 
+# =>   	Print "Please update!"
+		puts "Please update!"
+# =>    "Client info: client age" is equal to input prompt.
+		client_info[:client_age] = gets.chomp
+# =>    Print "Age has been updated to "client info: client age"
+		puts "Age has been updated to #{client_info[:client_age]}!"
+# => Elsif option is equal to "c" 
+	elsif option == "c" 
+# =>   	Print "Please update!"
+		puts "Please update!"
+# =>    "client info: client children" is equal to input prompt.
+		client_info[:client_children] = gets.chomp
+# =>    Print "Number of children has been updated to "client info: client children"
+		puts "Number of children has been updated to #{client_info[:client_children]}!"
+# => Elsif option is equal to "d" 
+	elsif option == "d" 
+# =>   	Print "Please update!"
+		puts "Please update!"
+# =>    "client info: decor theme" is equal to input prompt.
+		client_info[:decor_theme] = gets.chomp
+# =>    Print "Decor theme has been updated to "client info: decor theme"
+		puts "Decor theme has been updated to #{client_info[:decor_theme]}!"
+# => Else "answer" is equal to "no":
+	else answer == "no"
+# =>    print "Nothing to update!"
 		puts "Nothing to update at this time."
+# =>end method
+	end 
 #End 
 end 
 # Print "client info"
