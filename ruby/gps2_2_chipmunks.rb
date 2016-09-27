2362c7bca6b7daa9fd01738a978e1fa73684c78d
“Shana <realstarterv@gmail.com>
2016-09-27 14:35:24 -0700

    Add reflection questions and my answers

--- ruby/gps2_2_chipmunks.rb
+++ ruby/gps2_2_chipmunks.rb
@@ -29,7 +29,7 @@ list = create_list
 # input: a string for the item name and an integer for the quantity
 # steps: push item name and quantity into "item list" hash
 # output: "item list" hash
-def add_item(grocery_list, item,quantity = 1)
+def add_item(grocery_list, item, quantity = 1)
  	grocery_list[item.to_sym] = quantity
 end 
 
@@ -83,4 +83,29 @@ def print_list(list)
 	}
 end
 print_list(list)
+
+#REFLECT---------------------------
+# 1. What did you learn about pseudocode from working on this challenge?
+# I've learned a new way to layout my code. It takes some getting used to because I'm used
+# to writing mine a certain way but I can see how this way is useful, in how it separates
+# each chunk of what is done when pseudocoding a function. 
+
+# 2. What are the tradeoffs of using arrays and hashes for this challenge?
+# I think using arrays and hashes provide a good way of organizing and accessing information, 
+# especially for the type of list we were creating. As far as cons, the way we had it originally
+# written would split a single food item with two words such as "ice cream" into two separate
+# items, like "ice" and "cream". 
+
+# 3. What does a method return?
+# A method returns the result of what was done to the argument within the method. 
+
+# 4. What kind of things can you pass into methods as arguments?
+# You can pass variables, strings, or integers into methods as arguments. 
+
+# 5. How can you pass information between methods?
+# You can pass the information by putting the specific variable in a method's argument. 
+
+# 6. What concepts were solidified in this challenge, and what concepts are still confusing?
+# I got a better idea of how to access items in a hash and how to use block parameters. I can't
+# think of anything specifically within this challenge that I'm still confused about. 
 
