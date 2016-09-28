3d9a74828157e9920989d92ce7cb3a26a84712cd
“Shana <realstarterv@gmail.com>
2016-09-28 08:33:40 -0700

    Debug code

--- ruby/santa_chipmunks.rb
+++ ruby/santa_chipmunks.rb
@@ -1,5 +1,4 @@
-Santa class 
-
+class Santa 
 	def initialize
 		puts "Initializing new Santa instance...!"
 	end 
@@ -11,10 +10,9 @@ Santa class
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end 
-
 end 
 
 new_santa = Santa.new 
 
 new_santa.speak 
-new_santa.eat_milk_and_cookies
+new_santa.eat_milk_and_cookies("snickerdoodle")
