58acf3c725a9cd3737141f494bed1c609f41e486
“Shana <realstarterv@gmail.com>
2016-09-28 09:42:37 -0700

    Test calling attributes in methods

--- ruby/santa_chipmunks.rb
+++ ruby/santa_chipmunks.rb
@@ -1,25 +1,26 @@
 class Santa 
 #Instance methods
-	def initialize (gender, ethnicity)
+	def initialize(gender, ethnicity)
 		puts "Initializing new Santa instance...!"
 		@gender = gender
 		@ethnicity = ethnicity 
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
+			"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
 	end 
 
 	def speak 
 		puts "Ho, ho, ho! Haaaappy holidays!"
+		puts "This Santa is #{@gender} and #{@ethnicity}!"
 	end 
 
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
-	end 
- 
+	end  
 
 end 
 
-new_santa = Santa.new 
+new_santa = Santa.new("Female", "African-American") 
 
 new_santa.speak 
 new_santa.eat_milk_and_cookies("snickerdoodle")
