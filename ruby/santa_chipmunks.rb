e360c7eb8b38b5d642c834cc44532a211a6f341f
“Shana <realstarterv@gmail.com>
2016-09-28 08:25:36 -0700

    Define a Santa class with three instance methods, add code to check initialization

---
+++ ruby/santa_chipmunks.rb
@@ -0,0 +1,20 @@
+Santa class 
+
+	def initialize
+		puts "Initializing new Santa instance...!"
+	end 
+
+	def speak 
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end 
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end 
+
+end 
+
+new_santa = Santa.new 
+
+new_santa.speak 
+new_santa.eat_milk_and_cookies
