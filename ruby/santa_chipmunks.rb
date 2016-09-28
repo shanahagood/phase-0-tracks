a80b9d8586ca94299769f9b2b88233ae216fb972
“Shana <realstarterv@gmail.com>
2016-09-28 09:16:31 -0700

    Add attributes for gender, ethnicity, reindeer_ranking, and age

--- ruby/santa_chipmunks.rb
+++ ruby/santa_chipmunks.rb
@@ -1,6 +1,11 @@
 class Santa 
-	def initialize
+#Instance methods
+	def initialize (gender, ethnicity)
 		puts "Initializing new Santa instance...!"
+		@gender = gender
+		@ethnicity = ethnicity 
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end 
 
 	def speak 
@@ -10,6 +15,8 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end 
+ 
+
 end 
 
 new_santa = Santa.new 
