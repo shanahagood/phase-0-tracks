757a90133202352c7275a681fe72999ef0f824bd
“Shana <realstarterv@gmail.com>
2016-09-29 09:11:00 -0700

    debug

--- ruby/shout_chipmunks.rb
+++ ruby/shout_chipmunks.rb
@@ -2,10 +2,11 @@ module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end 
+
   def self.yelling_happily(words)
   	words + "!!!" + " :D"
   end
 end 
 
-self.yell_angrily("I hate you!")
-self.yelling_happily("I love you!")
+Shout.self.yell_angrily("I hate you!")
+Shout.self.yelling_happily("I love you!")
