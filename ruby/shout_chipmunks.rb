36970d0ea3bf1b1a14889f3e3e959d0a63f6818b
“Shana <realstarterv@gmail.com>
2016-09-29 08:35:47 -0700

    Add methods for yell_angirly and yelling_happily, test with driver code

--- ruby/shout_chipmunks.rb
+++ ruby/shout_chipmunks.rb
@@ -1,2 +1,10 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  def self.yelling_happily(words)
+  	words + "!!!" + " :D"
+  end
 end 
+
+self.yell_angrily("I hate you!")
+self.yelling_happily("I love you!")
