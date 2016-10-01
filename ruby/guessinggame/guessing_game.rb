a97315a21f8a4625b6d90ce292e713d8d1600403
“Shana <realstarterv@gmail.com>
2016-10-01 16:45:16 -0700

    Edit methods

--- ruby/guessinggame/guessing_game.rb
+++ ruby/guessinggame/guessing_game.rb
@@ -3,14 +3,14 @@ class Guessing_game
 
 	def initialiaze 
 		puts "initializing new game..."
-		@guess_count = 0 
+		@guess_count = guess_count 
 		@game_over = false 
 	end 
 
-	def mask_secret_word(secret_word)
-		secret_word = "_ " * secret_word.length
-		# puts secret_word
-	end 
+	# def mask_secret_word(secret_word)
+	# 	secret_word = "_ " * secret_word.length
+	# 	# puts secret_word
+	# end 
 
 	# def remaining_guesses
 	# 	while @guess_count != 0
@@ -18,34 +18,70 @@ class Guessing_game
 	# 	end 
 	# end 
 
-	def guesses_left(secret_word, guess)
-	  @guess_count = secret_word.length
-	  while @game_over != false 
-	  	@guess_count -= 1 
-	  	if secret_word.include? guess
-	  		puts "You guessed correctly!"
-	  	else 
-	  		puts "Sorry, but your guess was incorrect. Try again!"
-			puts "You have #{@guess_count} guesses remaining!"
-		# puts "Please enter in a letter!"
+
+
+	# def guesses_left
+	# 	@guess_count -= 1
+	# 	if @guess_count == 0 
+	# 		@game_over = true
+	# 	else 
+	# 		false  
+	# 	end 
+	# end 
+
+	# def guesses_left(secret_word, guess)
+	#   @guess_count = secret_word.length
+	#   while @game_over != false 
+	#   	@guess_count -= 1 
+	#   	if secret_word.include? guess
+	#   		puts "You guessed correctly!"
+	#   	else 
+	#   		puts "Sorry, but your guess was incorrect. Try again!"
+	# 		puts "You have #{@guess_count} guesses remaining!"
+	# 	# puts "Please enter in a letter!"
 		
-		end 
-	  end 
-	end 
+	# 	end 
+	#   end 
+	# end 
 
 end 
 
 game = Guessing_game.new 
+puts "Welcome to the guessing game!"
+
+secret_word_arr = []
 
 puts "Please enter in a secret word"
 secret_w_input = gets.chomp 
-puts game.mask_secret_word(secret_w_input)
+secret_w_input.split(' ')
+p secret_w_input 
+secret_word_arr << secret_w_input
+p secret_word_arr
+# puts game.mask_secret_word(secret_w_input)
 
-puts "Please guess a letter"
-letter_guess = gets.chomp 
+
+letter_guess_arr =[]
+
+while game.game_over != true 
+	puts "Please guess a letter"
+	letter_guess = gets.chomp 
+	letter_guess_arr << letter_guess 
+		if secret_word_arr.include? letter_guess
+			puts "You guessed right! Keep going!"
+			puts "You have guesses remaining!"
+		else 
+			puts "Sorry, but you guessed wrong! Try again!"
+			puts "You have guesses remaining!"
+		end 
+	end 
+# end 
+
+
 # puts game.remaining_guesses
 
-puts game.guesses_left(secret_w_input, letter_guess)
+
+
+# puts game.guesses_left(secret_w_input, letter_guess)
 
 # game.mask_secret_word("lemons")
 
