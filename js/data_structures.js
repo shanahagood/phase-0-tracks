3043dec4a9d6036856214eadd12fa2651712c7f3
“Shana <realstarterv@gmail.com>
2016-10-07 13:22:58 -0700

    Add code for release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -16,3 +16,20 @@ console.log(combined);
 //console.log(names);
 //console.log(colors);
 
+function Vehicle(make, model, year) {
+	console.log("New car:", this);
+
+	this.make = make;
+	this.model = model;
+	this.year= year;
+
+	this.honk = function() { console.log("Beeep!!!!"); };
+
+	console.log("VEHICLE INITIALIZATION COMPLETE");
+}
+
+var car = new Vehicle("Ford", "Focus", 2016);
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+car.honk();
