77c5de464400a866874de9642c2cb438f2d05d78
“Shana <realstarterv@gmail.com>
2016-10-07 12:52:23 -0700

    add code for release 1

---
+++ js/data_structures.js
@@ -0,0 +1,18 @@
+var combined = {},
+	i = 0,
+	names = ["Bob", "Sharon", "Apple", "Cinnamon"],
+	colors = ["blue", "green", "yellow", "red"];
+
+
+names.push("Buck")
+colors.push("Silver")
+
+for (i; i < names.length; i++){
+	combined[names[i]] = colors[i];
+}
+
+console.log(combined);
+
+//console.log(names);
+//console.log(colors);
+
