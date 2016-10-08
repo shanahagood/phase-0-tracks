ea71b71b5cde8ece9ea07a8331ce9cd1e1818405
“Shana <realstarterv@gmail.com>
2016-10-08 16:12:38 -0700

    Add pseudocode and declare function name

--- js/algos_chipmunks.js
+++ js/algos_chipmunks.js
@@ -4,4 +4,14 @@
 // on what you already know, rather than Googling the fanciest solution or looking up built-in sorting functions. 
 // Note that "some string".length will do what you expect in JS.
 
+//1. Declare a function called "return longest phrase" that takes an argument called "phrases" in its perameter. 
+//2. Iterate through each phrase in the array. 
+//3. Determine the length of each phrase. 
+//4. Print out the length of each phrase. 
+//5. Sort the phrase lengths from largest to smallest. This way, the largest phrase is at index 0. 
+//6. Print index 0. 
+
+function returnLongestPhrase(phrases) {
+	
+}
 
