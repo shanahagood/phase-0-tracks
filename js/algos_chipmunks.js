94097f93ec565749f31a744f4b5dc1d1510e2b8e
“Shana <realstarterv@gmail.com>
2016-10-08 19:21:25 -0700

    Add code that iterates through phrases and prints out the length of each

--- js/algos_chipmunks.js
+++ js/algos_chipmunks.js
@@ -6,12 +6,22 @@
 
 //1. Declare a function called "return longest phrase" that takes an argument called "phrases" in its perameter. 
 //2. Iterate through each phrase in the array. 
-//3. Determine the length of each phrase. 
-//4. Print out the length of each phrase. 
-//5. Sort the phrase lengths from largest to smallest. This way, the largest phrase is at index 0. 
+//3. Determine the longest phrase by printing out the length of each phrase.  
+//	Iterate through each index of "phrases".
+// 	Print out whichever index has the longest number. 
+
+
+//5. Sort the phrase lengths from largest to smallest. 
+//	
+//This way, the largest phrase is at index 0. 
 //6. Print index 0. 
 
 function returnLongestPhrase(phrases) {
+	// console.log(phrases);
+	for (var index = 0; index < phrases.length; index ++) {
+		console.log(phrases[index].length)
+	}
 	
 }
 
+console.log(returnLongestPhrase(["long phrase", "longest phrase", "longer phrase"]))
